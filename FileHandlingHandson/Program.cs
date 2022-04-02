6d4ad1d09a53f4a757538ae96cf75f7f5ec291ef
shubh <shubhdaghone@gmail.com>
2022-04-02 09:42:20 +0530

    New Filehandling update.

---
+++ FileHandlingHandson/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandlingHandson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHandling fileHandling = new FileHandling();
+            //fileHandling.WriteFile();
+            Console.WriteLine("Detailed");
+            Console.WriteLine("");
+            fileHandling.ReadFile();
+            Console.WriteLine("***************************************************************************************************************");
+            //Console.Read();
+
+
+        }
+        }
+    }
